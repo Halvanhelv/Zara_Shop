f0969de5f9380388d8d3a83466959b928848b68c
SynthesisOne <developerfromrussia@gmail.com>
2019-12-27 03:47:37 +0300

    mobile_category

--- public/menu/mobile_menu.php
+++ public/menu/mobile_menu.php
@@ -1,10 +1,7 @@
-<li>
-    <a href="category/<?=$category['alias'];?>"  ><?=$category['title'];?></a>
+<li><a href="category/<?=$category['alias'];?>"><?=$category['title'];?></a>
     <?php if(isset($category['childs'])): ?>
-    <ul>
-
-            <?= $this->getMenuHtml($category['childs'],$tab='',$tpl='1');?>
-
-    </ul>
+            <ul>
+                <?= $this->getMenuHtml($category['childs'],$tab='',$tpl='1');?>
+            </ul>
     <?php endif; ?>
-</li>
+        </li>
